9092fd5268f35b8cc92a73a1e37babc064ddf267
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-03-13 23:19:26 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            name: 'action',
+            message: 'What would you like to do?',
+            type: 'list',
+            choices: ['View all DEPARTMENTS', 'View all ROLES', 'View all EMPLOYEES', 'add a DEPARTMENT', 'add a ROLE', 'add an EMPLOYEE', 'update an EMPLOYEE ROLE'
+            ]
+        }
+    ])
